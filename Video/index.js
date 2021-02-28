371d9c56c54e1f0e08e42ff0fb92ca37de35742b
Stuti Mahajan <64328627+stutimahajan-97@users.noreply.github.com>
2021-03-01 00:04:15 +0530

    Create index.js

---
+++ Video/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/App';
+import App from './components/App';
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
